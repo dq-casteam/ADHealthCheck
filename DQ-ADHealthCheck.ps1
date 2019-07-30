b41ac6de9029cece90d1962ab6e161530cc5dbdc
dq-casteam <52086757+dq-casteam@users.noreply.github.com>
2019-07-30 13:14:56 -0400

    Update DQ-ADHealthCheck.ps1

--- DQ-ADHealthCheck.ps1
+++ DQ-ADHealthCheck.ps1
@@ -7,4 +7,5 @@ Write-Host ("[INFO] Creating Save Location $SaveLocation")
 New-Item -ItemType Directory -Path $SaveLocation -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri $Repository/$Script.ps1 -OutFile $SaveLocation\$Script.ps1 -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri $Repository/$Script-Run.ps1 -OutFile $SaveLocation\$Script-Run.ps1 -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-$SaveLocation\$Script-Run.ps1
+$ScriptToRun=$SaveLocation+"\"+$Script+"-Run.ps1"
+&$ScriptToRun
